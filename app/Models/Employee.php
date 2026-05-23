202ebfc0f399dd8eaf4f17ff0f505bc3f9f6a9ea
khadijaabirat <khadijaabirat@gmail.com>
2026-05-23 19:19:22 +0100

    feat(model): add Employee model for staff management

---
+++ app/Models/Employee.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'first_name',
+        'last_name',
+        'license_number',
+        'role',
+        'phone',
+        'is_active'
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function assignments()
+    {
+        return $this->hasMany(Assignment::class);
+    }
+}

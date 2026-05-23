f5d1b9bdd2e219893d245216e9d6b5431fbff549
khadijaabirat <khadijaabirat@gmail.com>
2026-05-23 19:19:27 +0100

    refactor(model): update Etape model with route step relationships

--- app/Models/Etape.php
+++ app/Models/Etape.php
@@ -6,17 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Etape extends Model
 {
-    public function route(){
+    protected $fillable = [
+        'ordre',
+        'heure_passage',
+        'route_id',
+        'gare_id'
+    ];
+
+    public function route()
+    {
         return $this->belongsTo(Route::class);
     }
-    public function gare(){
+
+    public function gare()
+    {
         return $this->belongsTo(Gare::class);
     }
-    public function segmentsDepart() {
-    return $this->hasMany(Segment::class, 'etape_depart_id');
-}
-public function segmentsArrivee() {
-    return $this->hasMany(Segment::class, 'etape_arrivee_id');
-}
 
+    public function segmentsDepart()
+    {
+        return $this->hasMany(Segment::class, 'etape_depart_id');
+    }
+
+    public function segmentsArrivee()
+    {
+        return $this->hasMany(Segment::class, 'etape_arrivee_id');
+    }
 }

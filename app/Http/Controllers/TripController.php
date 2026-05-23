2e1235050e30f4603b1ac7fd6ff703ac4ac64f36
khadijaabirat <khadijaabirat@gmail.com>
2026-05-23 19:19:27 +0100

    feat(trips): update TripController with segment-based search logic

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -2,33 +2,65 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Segment;
 use App\Models\Ville;
+use Carbon\Carbon;
 
 class TripController extends Controller
 {
-     public function index()
+    public function index()
     {
-        $villes = Ville::all();  
-        return view('search', compact('villes'));
+        $villes = Ville::orderBy('name')->get();
+        return view('trips.search', compact('villes'));
     }
- 
-public function search(Request $request)
-{
-    $trips = Segment::whereHas('depart.gare.ville', function($query) use ($request) {
-        $query->where('id', $request->departure_city);
-    })
-    ->whereHas('arrivee.gare.ville', function($query) use ($request) {
-        $query->where('id', $request->arrival_city);
-    })
-     ->whereHas('programme', function($query) use ($request) {
-        $query->whereDate('jour_depart', $request->travel_date);
-    })
-    ->with(['bus', 'depart.gare.ville', 'arrivee.gare.ville', 'programme'])
-    ->get();
 
-    return view('results', compact('trips'));
-}
+    public function search(Request $request)
+    {
+        $request->validate([
+            'departure_city' => 'required|exists:villes,id',
+            'arrival_city' => 'required|exists:villes,id|different:departure_city',
+            'travel_date' => 'required|date|after_or_equal:today',
+        ]);
+
+        $villes = Ville::orderBy('name')->get();
+        $departureCity = Ville::find($request->departure_city);
+        $arrivalCity = Ville::find($request->arrival_city);
+        $travelDate = Carbon::parse($request->travel_date);
+
+        $query = Segment::whereHas('depart.gare.ville', function($q) use ($request) {
+                $q->where('id', $request->departure_city);
+            })
+            ->whereHas('arrivee.gare.ville', function($q) use ($request) {
+                $q->where('id', $request->arrival_city);
+            })
+            ->whereHas('programme', function($q) use ($travelDate) {
+                $q->whereDate('jour_depart', $travelDate);
+            })
+            ->with(['bus', 'depart.gare.ville', 'arrivee.gare.ville', 'programme.route', 'reservations']);
+
+        // Filters
+        if ($request->filled('bus_type')) {
+            $query->whereHas('bus', function($q) use ($request) {
+                $q->whereIn('type', (array) $request->bus_type);
+            });
+        }
+
+        // Sorting
+        $sort = $request->input('sort', 'time_asc');
+        $trips = $query->get();
+
+        if ($sort === 'price_asc') {
+            $trips = $trips->sortBy('tarif');
+        } elseif ($sort === 'price_desc') {
+            $trips = $trips->sortByDesc('tarif');
+        } else {
+            // Default time_asc
+            $trips = $trips->sortBy(function($trip) {
+                return $trip->programme->heure_depart;
+            });
+        }
+
+        return view('trips.results', compact('trips', 'villes', 'departureCity', 'arrivalCity', 'travelDate'));
+    }
 }
